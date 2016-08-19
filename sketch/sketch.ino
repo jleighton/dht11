47bb9a14fa779e9017e0c7af93d9e4e62f4f98a1
James Leighton <github@jamesleighton.com>
2016-08-19 17:04:55 +0100

    working DHT11 results

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,13 +1,19 @@
 #include <SPI.h>
 #include <Ethernet.h>
+//DHT Stuff
+#include "DHT.h"
 
+
+
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT11   // DHT 11 
 byte mac[] = {
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
 };
 
 
 EthernetClient client;
-
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,12 +35,33 @@ void setup() {
   // print your local IP address:
   printIPAddress();
 
+  Serial.println("DHT11 Tester!");
+  
+dht.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+ // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  int h = dht.readHumidity();
+  int t = dht.readTemperature();
+
+  // check if returns are valid, if they are NaN (not a number) then something went wrong!
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+
+delay(2000);
 }
 
 void printIPAddress()
@@ -48,3 +75,6 @@ void printIPAddress()
 
   Serial.println();
 }
+
+
+
